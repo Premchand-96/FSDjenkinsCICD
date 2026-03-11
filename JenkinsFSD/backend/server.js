11899a8b1e699cd3475da763959c16fda62c141b
Premchand-96 <narsingpremchand@gmail.com>
2026-03-11 15:51:32 +0530

    Update server.js

--- JenkinsFSD/backend/server.js
+++ JenkinsFSD/backend/server.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.post("/contacts,(req,res)=>{
     const {name,email,phone} = req.body
 
-    const sql = "INSERT INTO contacts (name,email,phonenumberss) VALUES (?,?,?)"
+    const sql = "INSERT INTO contacts (name,email,phonenumbes) VALUES (?,?,?)"
 
     db.query(sql,[name,email,phone],(err,result)=>{
         if(err){
